5b782a89e2762f73e987cbe882affed8c478932e
musicakc <amitoj1606@gmail.com>
2015-04-13 22:21:17 +0530

    Adding iterative implementation

--- Numbers/factorial.py
+++ Numbers/factorial.py
@@ -1,13 +1,25 @@
-#Program to generate factorial of a number
+''' Factorial Finder - The Factorial of a positive integer, n,
+is defined as the product of the sequence n, n-1, n-2, ...1 and the
+factorial of zero, 0, is defined as being 1.
 
-def factorial(n):
+Recursive and iterative implementation.
+'''
+
+def factorialr(n):
     if n == 1:
         return 1
     else:
-        return n*factorial(n-1)
+        return n * factorialr(n-1)
+
+def factoriali(n):
+    prod = 1
+    for i in range(1, n+1):
+        prod = prod * i
+    return prod
 
 def main():
-    print(factorial(int(raw_input("Enter a number: "))))    
+    print(factorialr(int(raw_input("Enter a number: "))))
+    print(factoriali(int(raw_input("Enter a number: "))))
 
 
 if __name__ == "__main__":
